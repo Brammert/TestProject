9269c398b56041222933fc361f0c140acd5fd360
Bram Kleverlaan <bram@macaw.nl>
2013-01-28 11:09:47 +0100

    Added UnitTest

---
+++ trunk/SampleApplication/SampleClassLibrary/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleClassLibrary
+{
+    public static class MyClass
+    {
+        public static int DoMagic(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ trunk/SampleApplication/SampleUnitTestProject/MyClassTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using SampleClassLibrary;
+
+namespace SampleUnitTestProject
+{
+    [TestClass]
+    public class MyClassTest
+    {
+        [TestMethod]
+        public void TestDoMagic()
+        {
+            int expected = 3;
+            int actual = MyClass.DoMagic(1, 2);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
