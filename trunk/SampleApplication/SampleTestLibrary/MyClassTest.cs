47eaf116a72bfd2e8a258c2ddacee48b5f72c20f
Bram Kleverlaan <bram@macaw.nl>
2013-02-04 11:06:26 +0100

    Added nUnit to solution

---
+++ trunk/SampleApplication/SampleTestLibrary/MyClassTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using SampleClassLibrary;
+
+namespace SampleTestLibrary
+{
+    [TestFixture] 
+    public class MyClassTest
+    {
+        [Test]
+        public void MyAddTest()
+        {
+            Assert.AreEqual(MyClass.DoMagic(2, 3), 5);
+        }
+    }
+}
